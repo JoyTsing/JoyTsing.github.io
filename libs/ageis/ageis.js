0e3703fd31c3204de81d34fe00e0004c011661e9
JoyTsing <792566033@qq.com>
2024-04-11 19:11:16 +0800

    Site updated: 2024-04-11 19:11:14

--- libs/ageis/ageis.js
+++ libs/ageis/ageis.js
@@ -1,10 +1,11 @@
+// 引入后进行初始化
 import Aegis from 'aegis-web-sdk';
-    
-    const aegis = new Aegis({
-        id: 'Yrm6vSbWqbOJqn82xn', // 上报 id
-        uin: 'xxx', // 用户唯一 ID（可选）
-        reportApiSpeed: true, // 接口测速
-        reportAssetSpeed: true, // 静态资源测速
-        spa: true, // spa 应用页面跳转的时候开启 pv 计算
-        hostUrl: 'https://rumt-sg.com',
-    });
+
+const aegis = new Aegis({
+    id: 'KJ3Z9cqQKqVQvQkYQz', // 上报 id
+    uin: 'joyblog', // 用户唯一 ID（可选）
+    reportApiSpeed: true, // 接口测速
+    reportAssetSpeed: true, // 静态资源测速
+    spa: true, // spa 应用页面跳转的时候开启 pv 计算
+    hostUrl: 'https://rumt-sg.com',
+});
