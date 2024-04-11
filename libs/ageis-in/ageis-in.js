cd0f3c13320ad2f496be9f7fd147f92d5dc6691c
JoyTsing <792566033@qq.com>
2024-04-11 19:25:39 +0800

    Site updated: 2024-04-11 19:25:37

--- libs/ageis-in/ageis-in.js
+++ libs/ageis-in/ageis-in.js
@@ -2,7 +2,7 @@
 import Aegis from 'aegis-web-sdk';
 
 const aegis = new Aegis({
-    id: 'KJ3Z9cqQKqVQvQkYQz', // 上报 id
+    id: 'j4mOahLldLgWEvRo71', // 上报 id
     uin: 'joyblog', // 用户唯一 ID（可选）
     reportApiSpeed: true, // 接口测速
     reportAssetSpeed: true, // 静态资源测速
